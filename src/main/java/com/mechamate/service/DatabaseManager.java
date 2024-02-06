ab5637cd6c6dc749bdc483e68ba763dc20d76534
hansila <hansilakodagoda7781@gmail.com>
2024-02-06 12:49:21 +0530

    implement user profiles related database functions

--- src/main/java/com/mechamate/service/DatabaseManager.java
+++ src/main/java/com/mechamate/service/DatabaseManager.java
@@ -1,177 +1,176 @@
 package com.mechamate.service;
 
 import com.mechamate.entity.UserProfile;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
-import com.mongodb.MongoWriteException;
-import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-import io.github.cdimascio.dotenv.Dotenv;
+import com.mechamate.repo.UserProfileRepository;
+import com.mechamate.service.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
+@Service
 public class DatabaseManager {
-    private MongoClient mongoClient;
-    private MongoDatabase database;
-    private String hostName;
-    private String dbName;
-    private String username;
-    private String password;
     private Log log;
-    private Cache cache;
-    private boolean isReady;
 
-    public DatabaseManager(String hostName, String dbName, String username, String password, Log log) {
-        String source = this.getClass().getSimpleName() + "::DatabaseManager";
-        this.isReady = false;
+    private UserProfileRepository userProfileRepository;
+
+    @Autowired
+    public DatabaseManager(
+            Log log,
+            UserProfileRepository userProfileRepository) {
+        String source = this.getClass().getSimpleName() + "::DataBaseManager";
 
         if (log == null) {
             throw new IllegalArgumentException("Log cannot be null");
         }
+
         this.log = log;
 
-        if (hostName == null || hostName.trim().isEmpty()) {
-            log.log(Log.LogLevelEnum.LogError, source, "Host name is null or empty");
-        }
-        this.hostName = hostName;
+        log.log(Log.LogLevelEnum.LogDebug, source, "initializing DatabaseManager with repositories.");
 
-        if (dbName == null || dbName.trim().isEmpty()) {
-            log.log(Log.LogLevelEnum.LogError, source, "Database name is null or empty");
+        if (userProfileRepository == null) {
+            log.log(Log.LogLevelEnum.LogCritical, source, "userProfile Repository is null in DatabaseManager constructor");
         }
-        this.dbName = dbName;
 
-        if (username == null || username.trim().isEmpty()) {
-            log.log(Log.LogLevelEnum.LogError, source, "Username is null or empty");
-        }
-        this.username = username;
+        this.userProfileRepository = userProfileRepository;
+        log.log(Log.LogLevelEnum.LogDebug, source, "DatabaseManager initialized successfully");
+    }
 
-        if (password == null || password.trim().isEmpty()) {
-            log.log(Log.LogLevelEnum.LogError, source, "Password is null or empty");
+    public boolean addUserProfile(UserProfile userProfile) {
+        String source = this.getClass().getSimpleName() + "::addUserProfile";
+
+        if (userProfile == null) {
+            log.log(Log.LogLevelEnum.LogError,source,"UserProfile cannot be null");
+//            throw new IllegalArgumentException("UserProfile cannot be null");
+            return false;
         }
-        this.password = password;
+
+        // Check if the username already exists
+        if (userProfileRepository.findById(userProfile.getId()).isPresent()) {
+            log.log(Log.LogLevelEnum.LogError,source, "user profile ID already exists");
+//            throw new IllegalArgumentException("user profile ID already exist");
+            return false;
+        }
 
         try {
-            // initialize cache instance
-            this.cache = new Cache(log);
-            this.isReady = true;
-            log.log(Log.LogLevelEnum.LogDebug, source, "DatabaseManager initialized successfully");
+            // Save the new user profile
+            userProfileRepository.save(userProfile);
+            log.log(Log.LogLevelEnum.LogDebug, source, "user profile added successfully: " + userProfile.getUsername()+" with " +userProfile.getId());
+            return true;
         } catch (Exception e) {
-            log.log(Log.LogLevelEnum.LogError, source, "Failed to initialize Cache: " + e.getMessage());
-            throw new IllegalStateException("Failed to initialize Cache", e);
-        }
-    }
-    public boolean connectToDb() {
-        String source = this.getClass().getSimpleName() + "::connectToDb";
+            log.log(Log.LogLevelEnum.LogError, source, "error adding user profile: " + e.getMessage());
+//            throw new RuntimeException("Failed to add user profile", e);
 
-        if (isReady){
-            log.log(Log.LogLevelEnum.LogDebug, source, "DatabaseManager is ready");
-            try {
-                MongoCredential credential = MongoCredential.createCredential(username, dbName, password.toCharArray());
-                MongoClientSettings settings = MongoClientSettings.builder()
-                        .applyToClusterSettings(builder -> builder.hosts(Collections.singletonList(new ServerAddress(hostName))))
-                        .credential(credential)
-                        .build();
-                mongoClient = MongoClients.create(settings);
-                database = mongoClient.getDatabase(dbName);
-                log.log(Log.LogLevelEnum.LogDebug, source, "DatabaseManager connected successfully");
-                return true;
-            } catch (Exception e) {
-                log.log(Log.LogLevelEnum.LogError, source, "Could not connect to the database: " + e.getMessage());
-                return false;
-            }
-        } else {
-            log.log(Log.LogLevelEnum.LogCritical, source, "DatabaseManager is not ready");
             return false;
         }
     }
-    public boolean disconnectFromDb() {
-        String source = this.getClass().getSimpleName() + "::disconnectFromDb";
-        if (mongoClient != null) {
-            try {
-                mongoClient.close();
-                log.log(Log.LogLevelEnum.LogDebug, source, "Disconnected from the database successfully");
-                return true;
-            } catch (Exception e) {
-                log.log(Log.LogLevelEnum.LogError, source, "Error occurred while disconnecting the database: " + e.getMessage());
-                return false;
-            }
-        } else {
-            log.log(Log.LogLevelEnum.LogCritical, source, "No database connection was established");
-            return true; // No database to disconnect from
-        }
-    }
-    public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        String hostName = dotenv.get("DB_HOSTNAME");
-        String dbName = dotenv.get("DB_NAME");
-        String username = dotenv.get("DB_USERNAME");
-        String password = dotenv.get("DB_PASSWORD");
-        Log log = new Log(true, "src/test/log_records.txt", message -> {
-            System.out.println("Callback: " + message);
-        });
+    public UserProfile getUserProfile(String username) {
+        String source = this.getClass().getSimpleName() + "::getUserProfile";
 
-        if (hostName != null && dbName != null && username != null && password != null) {
-            DatabaseManager dbManager = new DatabaseManager(hostName, dbName, username, password, log);
-            if (dbManager.connectToDb()) {
-                dbManager.disconnectFromDb();
-            }
-        } else {
-            System.out.println("env variables are not set.");
+        if (username == null || username.trim().isEmpty()) {
+            log.log(Log.LogLevelEnum.LogError, source, "The username provided is null or empty.");
+            return null;
         }
-    }
-    //this method for ensure that database manager instance is ready to use
-    private MongoDatabase getDatabase() {
-        if (database == null && mongoClient != null) {
-            database = mongoClient.getDatabase(dbName);
-        }
-        return database;
-    }
 
-    //user profiles
-
-    public void addUserProfileToDb(UserProfile userProfile) {
-        String source = this.getClass().getSimpleName() + "::addUserProfileToDb";
         try {
-//            // data validation part
-//            if (userProfile.getUsername() == null || userProfile.getUsername().isEmpty()) {
-//                throw new IllegalArgumentException("Username cannot be null or empty");
-//            }
-//            if (userProfile.getPassword() == null || userProfile.getPassword().isEmpty()) {
-//                throw new IllegalArgumentException("Password cannot be null or empty");
-//            }
-
-            MongoDatabase db = getDatabase();
-
-            // check if userProfiles collection exists
-            boolean collectionExists = db.listCollectionNames()
-                    .into(new ArrayList<>())
-                    .contains("userProfiles");
-
-            if (!collectionExists) {
-                db.createCollection("userProfiles");
-                log.log(Log.LogLevelEnum.LogDebug, source, "userProfiles collection created");
-
-                // Create an index on the username field
-//                db.getCollection("userProfiles").createIndex(new Document("username", 1));
-//                log.log(Log.LogLevelEnum.LogDebug, source, "Index on 'username' created");
+            Optional<UserProfile> userProfile = userProfileRepository.findByUsername(username);
+            if (userProfile.isPresent()) {
+                log.log(Log.LogLevelEnum.LogDebug, source, "UserProfile retrieved successfully for username: " + username);
+                return userProfile.get();
+            } else {
+                log.log(Log.LogLevelEnum.LogError, source, "No UserProfile found for username: " + username);
+                return null;
             }
-
-//            Document newUserProfile = new Document("username", userProfile.getUsername())
-//                    .append("password", userProfile.getPassword());
-//            db.getCollection("userProfiles").insertOne(newUserProfile);
-//            log.log(Log.LogLevelEnum.LogDebug, source, "New user profile added");
-
-        } catch (IllegalArgumentException e) {
-            log.log(Log.LogLevelEnum.LogError, source, "Validation error: " + e.getMessage());
-        } catch (MongoWriteException e) {
-            log.log(Log.LogLevelEnum.LogError, source, "Error writing to the database: " + e.getMessage());
         } catch (Exception e) {
-            log.log(Log.LogLevelEnum.LogError, source, "An unexpected error occurred: " + e.getMessage());
+            log.log(Log.LogLevelEnum.LogError, source, "Error retrieving UserProfile: " + e.getMessage());
+            return null;
         }
     }
+    public boolean updateUserProfile(String id, UserProfile updatedUserProfile) {
+        String source = this.getClass().getSimpleName() + "::updateUserProfile";
 
+        if (updatedUserProfile == null) {
+            log.log(Log.LogLevelEnum.LogError, source, "Updated UserProfile object cannot be null.");
+            return false;
+        }
 
+        return userProfileRepository.findById(id)
+                .map(existingUserProfile -> {
+                    updateExistingUserProfile(existingUserProfile, updatedUserProfile);
+
+                    try {
+                        userProfileRepository.save(existingUserProfile);
+                        log.log(Log.LogLevelEnum.LogDebug, source, "UserProfile updated successfully for ID: " + id);
+                        return true;
+                    } catch (Exception e) {
+                        log.log(Log.LogLevelEnum.LogError, source, "Error updating UserProfile: " + e.getMessage());
+                        return false;
+                    }
+                })
+                .orElseGet(() -> {
+                    log.log(Log.LogLevelEnum.LogError, source, "UserProfile not found with ID: " + id);
+                    return false;
+                });
+    }
+
+    private void updateExistingUserProfile(UserProfile existingUserProfile, UserProfile updatedUserProfile) {
+        if (updatedUserProfile.getUsername() != null && !updatedUserProfile.getUsername().isEmpty()) {
+            existingUserProfile.setUsername(updatedUserProfile.getUsername());
+        }
+        if (updatedUserProfile.getPassword() != null && !updatedUserProfile.getPassword().isEmpty()) {
+            existingUserProfile.setPassword(updatedUserProfile.getPassword());
+        }
+        if (updatedUserProfile.getFirstName() != null && !updatedUserProfile.getFirstName().isEmpty()) {
+            existingUserProfile.setFirstName(updatedUserProfile.getFirstName());
+        }
+        if (updatedUserProfile.getLastName() != null && !updatedUserProfile.getLastName().isEmpty()) {
+            existingUserProfile.setLastName(updatedUserProfile.getLastName());
+        }
+        if (updatedUserProfile.getStatus() != null) {
+            existingUserProfile.setStatus(updatedUserProfile.getStatus());
+        }
+        if (updatedUserProfile.getVehicles() != null) {
+            existingUserProfile.setVehicles(updatedUserProfile.getVehicles());
+        }
+        //additional methods later
+
+    }
+    public boolean deleteUserProfile(String username) {
+        String source = this.getClass().getSimpleName() + "::deleteUserProfile";
+
+        if (username == null || username.trim().isEmpty()) {
+            log.log(Log.LogLevelEnum.LogError, source, "Username cannot be null or empty.");
+            return false;
+        }
+
+        // Retrieve the user profile by username
+        UserProfile userProfile = userProfileRepository.findByUsername(username)
+                .orElse(null);
+
+        if (userProfile == null) {
+            log.log(Log.LogLevelEnum.LogError, source, "No user profile found with username: " + username);
+            return false;
+        }
+
+        try {
+            userProfileRepository.delete(userProfile);
+            log.log(Log.LogLevelEnum.LogDebug, source, "User profile successfully deleted for username: " + username);
+            return true;
+        } catch (Exception e) {
+            log.log(Log.LogLevelEnum.LogError, source, "Error occurred while deleting user profile: " + e.getMessage());
+            return false;
+        }
+    }
+    public List<UserProfile> getAllUserProfiles(){
+        String source = this.getClass().getSimpleName() + "::getAllUserProfiles";
+        log.log(Log.LogLevelEnum.LogDebug,source,"Getting all user profiles");
+        try {
+            return userProfileRepository.findAll();
+        }catch (Exception e){
+            log.log(Log.LogLevelEnum.LogError, source, "Error occurred while retrieving all user profiles: " + e.getMessage());
+            return Collections.emptyList();
+        }
+    }
 }
