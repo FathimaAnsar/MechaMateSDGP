1d377b84c8622a4e975aa5af177a3f5b00064879
radeesh7 <radishka.20220963@iit.ac.lk>
2024-02-20 16:38:42 +0530

    Reset

---
+++ backend/src/main/java/com/mechamate/MechaMate.java
@@ -0,0 +1,20 @@
+package com.mechamate;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechaMate {
+
+	private static final Logger logger = LoggerFactory.getLogger(MechaMate.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechaMate.class, args);
+	}
+
+
+}
+
+
