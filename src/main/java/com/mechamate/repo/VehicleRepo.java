2bda95d99750fe8e3fc10aa2a8cd4ee7bc562925
radeesh7 <radishka.20220963@iit.ac.lk>
2024-02-20 16:07:56 +0530

    REPO RE-STRUCTURED!

--- src/main/java/com/mechamate/repo/VehicleRepo.java
+++
@@ -1,7 +0,0 @@
-package com.mechamate.repo;
-
-import com.mechamate.entity.Vehicle;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface VehicleRepo extends MongoRepository<Vehicle, Long> {
-}
