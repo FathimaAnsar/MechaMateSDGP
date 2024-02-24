dd36aa1c5a32be5eea1c384c1ec645c8add2ca98
hansila <hansilakodagoda7781@gmail.com>
2024-02-24 18:30:47 +0530

    implement sessions related CacheManager methods

--- backend/src/main/java/com/mechamate/service/CacheManager.java
+++ backend/src/main/java/com/mechamate/service/CacheManager.java
@@ -12,12 +12,13 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CacheManager {
     private static final Logger logger = LoggerFactory.getLogger(MechaMate.class);
     private final ConcurrentHashMap<String, UserProfile> userProfileCache = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<String, Session> SessionCache = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Session> sessionCache = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, Vehicle> VehicleCache = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, ServiceRecord> ServiceRecordCache = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, Maintenance> MaintenanceCache = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<String, ServiceRecord> PredictionModelCache = new ConcurrentHashMap<>();
 
+    //********************************** user profiles ************************************************
     public boolean isUserExistInCache (String key){
         boolean isExist = false;
         if (key == null) {
@@ -91,6 +92,79 @@ public class CacheManager {
             logger.error("exception occurred while deleting UserProfile from cache. Key: {}, Exception: {}", key, e.toString());
         }
     }
+    //************************************* sessions ************************************************
+
+    public boolean isSessionExistInCache (String key){
+        boolean isExist = false;
+        if (key == null) {
+            logger.warn("attempt to get a session with a null key.");
+        }
+        try {
+            isExist = sessionCache.containsKey(key);
+            logger.info("found session for key: {}", key);
+
+        }catch (Exception e){
+            logger.error("exception happened while retrieving session from cache for key: {}. Exception: {}", key, e.toString());
+        }
+        return isExist;
+    }
+
+    public Session getFromSessionCache(String key) {
+
+        if (key == null) {
+            logger.warn("attempt to get a session with a null key.");
+            return null;
+        }
+
+        try {
+
+            Session session = sessionCache.get(key);
+
+            if (session == null) {
+                logger.info("cache miss for session key: {}", key);
+                return null;
+            } else {
+                logger.info("found session for key: {}", key);
+                return session;
+            }
+        } catch (Exception e) {
+            logger.error("exception happened while retrieving session from cache for key: {}. Exception: {}", key, e.toString());
+            return null;
+        }
+    }
 
 
+    public void putInSessionCache(String key, Session session) {
+        if (key == null || session == null) {
+            logger.warn("attempt to put a null key or session into the cache. Key: {}, UserProfile: {}", key, session);
+            return;
+        }
+
+        try {
+            sessionCache.put(key, session);
+            logger.info("session put into cache under key: {}", key);
+        } catch (Exception e) {
+            logger.error("exception happens while putting session into cache. Key: {}, Exception: {}", key, e.toString());
+        }
+    }
+
+
+    public void deleteFromSessionCache(String key) {
+        if (key == null) {
+            logger.warn("attempt to delete session from cache with null key.");
+            return;
+        }
+
+        try {
+            if (!sessionCache.containsKey(key)) {
+                logger.info("attempt to delete a non session key from cache: {}", key);
+                return;
+            }
+            sessionCache.remove(key);
+            logger.info("session removed from cache for key: {}", key);
+
+        } catch (Exception e) {
+            logger.error("exception occurred while deleting session from cache. Key: {}, Exception: {}", key, e.toString());
+        }
+    }
 }
