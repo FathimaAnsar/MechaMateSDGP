31915325861eace5811f23281346ff910fc11889
radeesh7 <radishka.20220963@iit.ac.lk>
2024-02-28 09:42:45 +0530

    Fixed MechaMate main class

--- backend/src/main/java/com/mechamate/MechaMate.java
+++ backend/src/main/java/com/mechamate/MechaMate.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MechaMate implements WebMvcConfigurer {
+public class MechaMate {
 	private static final Logger logger = LoggerFactory.getLogger(MechaMate.class);
 
 	public static void main(String[] args) {
