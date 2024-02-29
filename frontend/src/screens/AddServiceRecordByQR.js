807fe9aabd3f4c471233c88546a9eae4e30e49b7
chamathjayasanka <135022486+chamathjayasanka@users.noreply.github.com>
2024-02-29 17:17:51 +0530

    created the advised service record by Qr

--- frontend/src/screens/AddServiceRecordByQR.js
+++ frontend/src/screens/AddServiceRecordByQR.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import QRCode from 'react-qr-code';
+
 function AddServiceRecordByQR(props) {
     return (
-        <>
-            <h2>Add Service Record by QR</h2>
-            // Content and form for adding service records by scanning QR
-            <button onClick={() => props.app.changePage('ManageMyDocuments')}>Go Back</button>
-        </>
+        <div className="add-record-container">
+        <div className="add-manually-button">
+          <Button variant="primary" onClick={() => window.open(url, '_blank')}>Add manually On this device</Button>
+        </div>
+        <h6>or</h6>
+        <h1>Scan QR Code</h1>
+        <h6>Scan this QRCode to Open Service Record form</h6>
+        <div className="qr-code-section">
+  
+          <div className="qr-code-wrapper">
+            <QRCode value={url} />
+          </div>
+        </div>
+      </div>
     );
 }
 
 export default AddServiceRecordByQR;
+
