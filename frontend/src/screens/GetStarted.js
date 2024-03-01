5d01970de8ef96e0d007ef78896d55b7c59bb814
gihan <gihanm.jaya@gmail.com>
2024-03-02 01:18:51 +0530

    updates in login ui

--- frontend/src/screens/GetStarted.js
+++ frontend/src/screens/GetStarted.js
@@ -2,10 +2,6 @@ import React, { useState } from "react";
 import { Container, Button, Card, CardFooter, CardImg } from 'react-bootstrap';
 import logo from '../images/logo-white.png';
 import { Pages } from "../Pages.js"
-import '../styles/App.css';
-
-
-import '../styles/App.css';
 
 function GetStarted(props) {
 
@@ -18,7 +14,7 @@ function GetStarted(props) {
                     <Card.Img variant="top" src={logo} style={{ width: '250px' }} />
                 </Card.Body>
                 <Card.Footer className='d-flex  justify-content-center' style={{ width: '100vw', marginBottom:'5px',borderTop: '0', backgroundColor: 'transparent' }}>
-                    <Button style={{ backgroundColor: 'white', width: '300px', color: '#085bd4', fontWeight: '500', borderRadius: '30px', padding: '6px' }}
+                    <Button style={{ backgroundColor: 'white', width: '300px', color: '#085bd4', fontSize: '12pt', fontWeight: '700', borderRadius: '30px', padding: '6px' }}
                             onClick={() => handleClick(Pages.SignInUI)}>
                         Let's Get Started
                     </Button>
