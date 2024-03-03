db8ced0c8dc3e14560c7a2c41b18e15044d0254a
FathimaAnsar <fathimaansar295@gmail.com>
2024-03-03 18:34:53 +0530

    add vehicle

--- frontend/src/screens/MyVehicles.js
+++ frontend/src/screens/MyVehicles.js
@@ -1,47 +1,131 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Header from "./components/Header";
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import DatePicker from "react-datepicker"; 
+import Button from 'react-bootstrap/Button';
+import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
+
+
+
 
 function MyVehicles(props) {
+  const [_id, setId] =useState(null);
+  const [vehicleType, setVehicleType] = useState('Car');
+  const [fuelType, setFuelType] = useState(null);
+  const [vehicleMake, setVehicleMake] = useState(null);
+  const [vehicleModel, setVehicleModel] = useState(null);
+  const [regNo, setRegNo] = useState(null);
+  const [regExpDate, setRegExpDate] = useState(new Date());
+  const [insNo, setInsNo] = useState(null);
+  const [insExpDate, setInsExpDate] = useState(new Date());
   const handleGoBack = () => { props.app.goBack(); }
+
+  // add vehicle with api logic
+  async function addVehicle(event)
+  {
+      event.preventDefault();
+  try
+      {
+       await axios.post("http://localhost:8080/api/v1/general/add-vehicle",
+      {
+        regNo: regNo,
+        vehicleType: vehicleType,
+        fuelType: fuelType,
+        vehicleMake: vehicleMake,
+        vehicleModel: vehicleModel,
+        insNo: insNo,
+        insExpDate: insExpDate,
+        regExpDate: regExpDate
+      });
+        alert("Vehicle Registation Successful");
+        //setId("");
+        setRegNo("");
+        setVehicleType("");
+        setFuelType("");
+        setVehicleMake("");
+        setVehicleModel("");
+        setInsNo("");
+        setInsExpDate("");
+        setRegExpDate("");
+
+      }
+  catch(err)
+      {
+        alert("Vehicle Registation Failed");
+      }
+ }
+
   return (
     <div>
-        <form>
-  <label for="vehicleType">Vehicle Type:</label>
-  <select id="vehicleType" name="vehicleType">
+    <Header/>
+    <Form>
+    <FloatingLabel controlId="floatingSelect" label="Vehicle Type" className='dropdown'>
+    <Form.Select aria-label="Floating label select example"  value={vehicleType}
+      onChange={(event) => {
+        setVehicleType(event.target.value);
+      }}>
+    <option>Select Vehicle Type</option>
     <option value="Car">Car</option>
     <option value="Van">Van</option>
     <option value="Bus">Bus</option>
     <option value="Lorry">Lorry</option>
     <option value="Motorcycle">Motorcycle</option>
-  </select><br/>
+
+  </Form.Select>
+  </FloatingLabel>
   
-  <label for="fuelType">Fuel Type:</label>
-  <select id="fuelType" name="fuelType">
+  <FloatingLabel controlId="floatingSelect" label="Fuel Type" className='dropdown'>
+    <Form.Select aria-label="Floating label select example" value={fuelType}
+    onChange={(event) => {
+      setFuelType(event.target.value);
+    }}>
+    <option>Select Fuel Type</option>
     <option value="Petrol">Petrol</option>
     <option value="Diesel">Diesel</option>
     <option value="LPGas">LPGas</option>
     <option value="Electric">Electric</option>
-  </select><br/>
-  
-  <label for="vehicleMake">Vehicle Make:</label>
-  <input type="text" id="vehicleMake" name="vehicleMake" placeholder="Toyota"/><br/>
-  
-  <label for="vehicleModel">Vehicle Model:</label>
-  <input type="text" id="vehicleModel" name="vehicleModel" placeholder="Camry"/><br/>
-  
-  <label for="regNo">Vehicle Registration Number:</label>
-  <input type="text" id="regNo" name="regNo" placeholder="ABC1234"/><br/>
-  
-  <label for="regExpDate">Vehicle Registration Expiration Date:</label><br/>
-  <input type="date" id="regExpDate" name="regExpDate"/>
-  
-  <label for="insNo">Vehicle Insurance Policy Number:</label>
-  <input type="text" id="insNo" name="insNo" placeholder="ABC123456789"/><br/>
-  
-  <label for="insExpDate">Vehicle Insurance Expiration Date:</label>
-  <input type="date" id="insExpDate" name="insExpDate"/><br/>
-  
-  <button type="submit">Save</button>
-</form>
+  </Form.Select>
+  </FloatingLabel>
+ 
+  <Form.Label>Vehicle Make</Form.Label>
+  <Form.Control className='textbox' type="text" placeholder="Toyota" value={vehicleMake}
+  onChange={(event)=>{
+    setVehicleMake(event.target.value);
+  }}
+  />
+  <Form.Label>Vehicle Model</Form.Label>
+  <Form.Control className='textbox'  type="text" placeholder="Camry" value={vehicleModel} 
+  onChange={(event)=>{
+    setVehicleModel(event.target.value);
+  }}
+  />
+  <Form.Label>Vehicle Registration Number</Form.Label>
+  <Form.Control className='textbox'  type="text" placeholder="ABC1234" value={regNo}
+  onChange={(event)=>{
+    setRegNo(event.target.value);
+  }}/>
+
+  <Form.Group controlId="vred">
+  <Form.Label>Vehicle Registratration Expiration Date:  </Form.Label> 
+  <DatePicker selected={regExpDate} onChange={(date) => setRegExpDate(date)} /><br/>
+  </Form.Group>
+
+  <Form.Label>Vehicle Insurance Policy Number</Form.Label>
+  <Form.Control className='textbox'  type="text" placeholder="ABC123456789" value={insNo}
+  onChange={(event)=>{
+    setInsNo(event.target.value);
+  }}/>
+
+
+  <Form.Group controlId="vied">
+  <Form.Label>Vehicle Insurance Expiration Date:  </Form.Label> 
+  <DatePicker selected={insExpDate} onChange={(date) => setInsExpDate(date)} />
+  </Form.Group>
+
+  <Button variant="primary" onClick={addVehicle}>Save</Button> 
+  </Form>
 
 <button onClick={handleGoBack}>Go Back</button>
 
