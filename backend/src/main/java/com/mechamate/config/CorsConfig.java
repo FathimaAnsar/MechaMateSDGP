d9330a957236b1589afb01640798c5edb5112186
hansila <hansilakodagoda7781@gmail.com>
2024-03-06 14:55:22 +0530

    testing android app on a device

--- backend/src/main/java/com/mechamate/config/CorsConfig.java
+++ backend/src/main/java/com/mechamate/config/CorsConfig.java
@@ -10,7 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+//                        "http://localhost:3000",
+                        "https://mechamate-413916.el.r.appspot.com",
+                        "capacitor://localhost")
                 .allowCredentials(true);
     }
 }
