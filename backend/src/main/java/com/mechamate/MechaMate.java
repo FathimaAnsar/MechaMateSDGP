06ae99d03f44e8922d209dd02c9b7f8f30a777b5
radeesh7 <radishka.20220963@iit.ac.lk>
2024-03-13 02:24:07 +0530

    updated predictive maintenance class

--- backend/src/main/java/com/mechamate/MechaMate.java
+++ backend/src/main/java/com/mechamate/MechaMate.java
@@ -1,9 +1,15 @@
 package com.mechamate;
 
+import com.mechamate.entity.Maintenance;
+import com.mechamate.entity.PredictionModel;
+import com.mechamate.features.PdM;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class MechaMate {
@@ -11,7 +17,24 @@ public class MechaMate {
 	private static final Logger logger = LoggerFactory.getLogger(MechaMate.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(MechaMate.class, args);
+
+
+	//	double x = 248.5;
+
+		List<Maintenance.MaintenanceType> maintenanceTypes = new ArrayList<>();
+
+		maintenanceTypes.add(Maintenance.MaintenanceType.EngineOilChange);
+
+		PredictionModel predictionModel = PdM.getTrainedPredictionModel(
+				"engine-heat-dataset.csv", "Engine Oil",
+				"This model applies to predict lifespan of engine oil",maintenanceTypes);
+
+		System.out.println(predictionModel.toString());
+
+		return;
+
+//		SpringApplication.run(MechaMate.class, args);
+
 	}
 
 
--- backend/src/main/java/com/mechamate/features/PdM.java
+++ backend/src/main/java/com/mechamate/features/PdM.java
@@ -5,26 +5,54 @@ import com.mechamate.entity.Maintenance;
 import com.mechamate.entity.PredictionModel;
 import com.mechamate.entity.UserProfile;
 import com.mechamate.entity.Vehicle;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 @Service
 public class PdM {
 
 
+    @Value("${mechamate.dataset.dir}")
+    private static String datasetDir = "../datasets/";
+
+    public double predict(double[] coefficients, double x) {
+        if (coefficients == null || coefficients.length != 2) {
+            throw new IllegalArgumentException("Coefficients array must be of length 2.");
+        }
+        double m = coefficients[0];
+        double c = coefficients[1];
+        return m * x + c;
+    }
+
 
     public PredictionModel getTrainedPredictionModel(String csvFileName, String name, String description,
                                                      List<Maintenance.MaintenanceType> appliedMaintenanceList) {
-        double m = 0;
-        double c = 0;
 
+        SimpleRegression regression = new SimpleRegression();
+        try (Scanner scanner = new Scanner(new FileReader(System.getProperty("user.dir") + "/datasets/" + csvFileName))) {
+            while (scanner.hasNextLine()) {
+                try {
+                    String[] data = scanner.nextLine().split(",");
+                    double x = Double.parseDouble(data[0]);
+                    double y = Double.parseDouble(data[1]);
+                    regression.addData(x, y);
+                } catch (Exception e) {}
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
 
-
-
-        PredictionModel predictionModel = new PredictionModel(name, description, m, c, appliedMaintenanceList);
+        PredictionModel predictionModel = new PredictionModel(name, description, regression.getSlope(),
+                regression.getIntercept(), appliedMaintenanceList);
         return predictionModel;
     }
 
