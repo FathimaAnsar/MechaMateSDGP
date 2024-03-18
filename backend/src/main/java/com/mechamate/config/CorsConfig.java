01dadb51e25fd8fc4474a632e387dcd48f40f8a6
hansila <hansilakodagoda7781@gmail.com>
2024-03-18 22:12:32 +0530

    add test constructor

--- backend/src/main/java/com/mechamate/config/CorsConfig.java
+++ backend/src/main/java/com/mechamate/config/CorsConfig.java
@@ -7,15 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins(
-//                        "http://localhost:3000", // For local development
-//                        "https://mechamate-413916.el.r.appspot.com", // production server
-//                        "capacitor://localhost", // iOS capacitor,
-//                        "http://localhost") // android capacitor
-//                .allowCredentials(true);
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000", // For local development
+                        "https://mechamate-413916.el.r.appspot.com", // production server
+                        "capacitor://localhost", // iOS capacitor,
+                        "http://localhost") // android capacitor
+                .allowCredentials(true);
+    }
 }
 
