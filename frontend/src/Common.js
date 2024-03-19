1ba39ea24be91fdf88bd67f247ab427059e8e364
hansila <hansilakodagoda7781@gmail.com>
2024-03-19 22:08:07 +0530

    testing ci/cd

--- frontend/src/Common.js
+++ frontend/src/Common.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8080";
+// export const API_BASE_URL = "http://localhost:8080";
 // export const API_BASE_URL = "https://mechamate-413916.el.r.appspot.com";
-// export const API_BASE_URL = "http://mechamate.site";
+export const API_BASE_URL = "https://mechamate.site";
 
