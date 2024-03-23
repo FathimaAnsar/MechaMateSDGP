6b8f583947949f01a81e60ec0bf7f18b58b16d96
gihan <gihanm.jaya@gmail.com>
2024-03-24 00:00:36 +0530

    updates in front end

--- frontend/src/screens/components/ToggleThemeButton.js
+++ frontend/src/screens/components/ToggleThemeButton.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import ThemeContext  from './ThemeContext'; 
+import ThemeContext from './ThemeContext';
 
 function ToggleThemeButton() {
   const { theme, toggleTheme } = React.useContext(ThemeContext);
 
   return (
     <Button
-      variant={theme === 'light' ? 'dark' : 'light'}
+      variant={theme === 'light' ? 'light' : 'dark'}
       onClick={toggleTheme}
     >
-      {theme === 'light' ? '☀️' : ' 🌑'}
+      {theme === 'light' ? '☀️' : '🌑'}
     </Button>
   );
 }
