ba4a4ddfe1bc76ca61a4a2aad2671f092c317e6a
hansila <hansilakodagoda7781@gmail.com>
2024-03-24 21:43:35 +0530

    fix the EndpointHost

--- backend/src/main/java/com/mechamate/config/CorsConfig.java
+++ backend/src/main/java/com/mechamate/config/CorsConfig.java
@@ -7,14 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins(
-////                        "http://localhost:3000", // For local development
-//                        "capacitor://localhost", // iOS capacitor,
-//                        "http://localhost") // android capacitor
-//                .allowCredentials(true);
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("capacitor://localhost")
+                .allowCredentials(true);
+    }
 }
 
