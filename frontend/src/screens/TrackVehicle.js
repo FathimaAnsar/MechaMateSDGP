ac785c659ba683b784a6c290e0002a2e1340aee7
hansila <hansilakodagoda7781@gmail.com>
2024-03-25 05:51:07 +0530

    update ui

--- frontend/src/screens/TrackVehicle.js
+++ frontend/src/screens/TrackVehicle.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Accordion, Container, Row, Col, Button, Form, Card } from "react-bootstrap";
+import React, {useState, useEffect} from "react";
+import {Accordion, Container, Row, Col, Button, Form, Card} from "react-bootstrap";
 import ConnectionManager from "../services/ConnectionManager";
 import Header from "./components/Header";
 import CustomAlert from "./components/CustomAlert";
-import { ClipLoader } from "react-spinners";
-// import "./styles/TrackVehicle.css";
+import {ClipLoader} from "react-spinners";
 
 function TrackVehicle(props) {
     const [vehicles, setVehicles] = useState([]);
@@ -12,7 +11,7 @@ function TrackVehicle(props) {
     const [vehicleInfo, setVehicleInfo] = useState(null);
     const [policeStations, setPoliceStations] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [alertInfo, setAlertInfo] = useState({ show: false, error: { heading: '', message: '' } });
+    const [alertInfo, setAlertInfo] = useState({show: false, error: {heading: '', message: ''}});
     const [dropdownActive, setDropdownActive] = useState(false);
     const [showPoliceStations, setShowPoliceStations] = useState(false);
 
@@ -21,11 +20,12 @@ function TrackVehicle(props) {
             setLoading(true);
             try {
                 const vehicleList = await props.app.getVehicleList();
-                setVehicles(vehicleList || []);
+                if (!vehicleList) throw new Error("Received no data");
+                setVehicles(vehicleList);
             } catch (error) {
                 setAlertInfo({
                     show: true,
-                    error: { heading: 'Loading Error', message: 'Failed to load vehicles.' }
+                    error: {heading: 'Loading Error', message: error.message || 'Failed to load vehicles.'}
                 });
             } finally {
                 setLoading(false);
@@ -34,13 +34,6 @@ function TrackVehicle(props) {
 
         fetchVehicles();
     }, [props.app]);
-
-    const togglePoliceStations = async () => {
-        setShowPoliceStations(!showPoliceStations);
-        if (!showPoliceStations && !policeStations.length) {
-            await showNearbyPoliceStations();
-        }
-    };
 
     const trackVehicle = async () => {
         if (!selectedVehicleRegNo) {
@@ -58,32 +51,30 @@ function TrackVehicle(props) {
         let connection = new ConnectionManager();
         try {
             const response = await connection.getVehicleLocation(selectedVehicleRegNo);
+            if (!response) throw new Error("Received no data");
             const vehicleLocationData = JSON.parse(response);
-            if (vehicleLocationData && vehicleLocationData.vehicleRegNo) {
-                setVehicleInfo(vehicleLocationData);
-            } else {
-                setAlertInfo({
-                    show: true,
-                    error: { heading: 'Tracking Error', message: "Could not find vehicle data." }
-                });
-            }
+            if (!vehicleLocationData.vehicleRegNo) throw new Error("Vehicle data is incomplete.");
+            setVehicleInfo(vehicleLocationData);
+
+            await showNearbyPoliceStations(vehicleLocationData.latitude, vehicleLocationData.longitude);
         } catch (error) {
             setAlertInfo({
                 show: true,
-                error: { heading: 'Tracking Error', message: error.message || "Could not track the vehicle." }
+                error: {heading: 'Tracking Error', message: error.message || 'Could not track the vehicle.'}
             });
         } finally {
             setLoading(false);
         }
     };
 
-    const showNearbyPoliceStations = async () => {
-        if (!vehicleInfo) {
+
+    const showNearbyPoliceStations = async (latitude, longitude) => {
+        if (!latitude || !longitude) {
             setAlertInfo({
                 show: true,
                 error: {
-                    heading: 'Vehicle Location Required',
-                    message: 'Please track your vehicle to find the nearest police stations.'
+                    heading: 'Location Error',
+                    message: 'No location data available to find police stations.'
                 }
             });
             return;
@@ -92,41 +83,53 @@ function TrackVehicle(props) {
         setLoading(true);
         let connection2 = new ConnectionManager();
         try {
-            const responsePoliceStations = await connection2.getNearbyPoliceStations(
-                vehicleInfo.latitude,
-                vehicleInfo.longitude,
-                10000,
-                10
-            );
+            const responsePoliceStations = await connection2.getNearbyPoliceStations(latitude, longitude, 10000, 10);
+            if (!responsePoliceStations) throw new Error("Received no data");
             const responsePoliceStationsData = JSON.parse(responsePoliceStations);
+            if (!responsePoliceStationsData.places) throw new Error("No police stations data.");
             setPoliceStations(responsePoliceStationsData.places);
         } catch (error) {
             setAlertInfo({
                 show: true,
-                error: { heading: 'Error', message: 'Failed to retrieve police stations.' }
+                error: {heading: 'Error', message: error.message || 'Failed to retrieve police stations.'}
             });
         } finally {
             setLoading(false);
         }
     };
-
     const contactPolice = () => {
-        window.location.href = "tel:+911";
+        const stationWithPhone = policeStations.find(station => station.nationalPhoneNumber);
+        if (stationWithPhone) {
+            window.location.href = `tel:${stationWithPhone.nationalPhoneNumber}`;
+        } else {
+            setAlertInfo({
+                show: true,
+                error: {
+                    heading: 'Contact Error',
+                    message: 'No police station with a contact number found.'
+                }
+            });
+        }
     };
 
-    const handleCloseAlert = () => setAlertInfo({ show: false, error: { heading: '', message: '' } });
+    const handleCloseAlert = () => setAlertInfo({show: false, error: {heading: '', message: ''}});
 
+    const togglePoliceStations = () => {
+        setShowPoliceStations(!showPoliceStations);
+    };
 
     return (
         <>
-            <Header app={props.app} />
-            <Container id="trackVehicleContainer">
-                <Row className="mt-3">
-                    <Col className="text-center">
-                        <h2 className="mb-4">Track My Vehicle</h2>
+            <Header app={props.app}/>
+            <Container id="tmvContainer">
+                <Row id="tmvTitleRow" className="mt-3">
+                    <Col id="tmvTitleCol" className="text-center">
+                        <h2 id="tmvTitle">Track My Vehicle</h2>
                         <Form.Group controlId="vehicleSelect">
                             <Form.Label>Select Your Vehicle</Form.Label>
-                            <Form.Control
+                            <Form.Select
+                                size="lg"
+                                style={{marginTop:"25px"}}
                                 as="select"
                                 value={selectedVehicleRegNo}
                                 onChange={e => setSelectedVehicleRegNo(e.target.value)}
@@ -139,13 +142,14 @@ function TrackVehicle(props) {
                                         {vehicle.registrationNumber} - {vehicle.vehicleMake} {vehicle.vehicleModel}
                                     </option>
                                 ))}
-                            </Form.Control>
+                            </Form.Select>
                         </Form.Group>
                         <Button
-                            variant="primary"
+                            style={{ margin: '40px' }}
+                            id="tmvTrackNowButton"
+                            variant="outline-primary"
                             onClick={trackVehicle}
                             disabled={loading || vehicles.length === 0}
-                            className={dropdownActive ? "mt-3" : ""}
                         >
                             Track Now
                         </Button>
@@ -153,85 +157,96 @@ function TrackVehicle(props) {
                 </Row>
 
                 {loading && (
-                    <Row className="mt-3 justify-content-center">
+                    <Row id="tmvLoadingRow" className="mt-3 justify-content-center">
                         <Col className="text-center">
-                            <ClipLoader color="#007bff" loading={loading} size={150} />
+                            <ClipLoader color="#007bff" loading={loading} size={150}/>
                         </Col>
                     </Row>
                 )}
 
                 {!loading && vehicleInfo && vehicleInfo.vehicleRegNo && (
                     <>
-                        <Row className="mt-3">
+                        <Row id="tmvMapRow" className="mt-3">
                             <Col sm={12}>
                                 <Card>
-                                    <Card.Body>
+                                    <Card.Body id="tmvMapCardBody">
                                         <iframe
-                                            className="map-container"
+                                            id="tmvMapContainer"
                                             title="vehicleLocationMap"
                                             src={`https://maps.google.com/maps?q=${vehicleInfo.latitude},${vehicleInfo.longitude}&z=15&output=embed`}
                                             frameBorder="0"
+                                            width="100%"
+                                            height="300"
+                                            style={{border: 0}}
                                             allowFullScreen
                                         ></iframe>
-                                        <div className="mt-2">
-                                            <p>Location Date Time: {vehicleInfo.locationDateTime || 'Not available'}</p>
-                                            <p>Device Online: {vehicleInfo.deviceOnline ? "Yes" : "No"}</p>
-                                            <p>Engine Running: {vehicleInfo.engineRunning ? "Yes" : "No"}</p>
+                                        <div id="tmvVehicleInfo" className="mt-2">
+                                            <p id="tmvLocationDateTime">Location Date Time: {vehicleInfo.locationDateTime || 'Not available'}</p>
+                                            <p id="tmvDeviceOnline">Device Online: {vehicleInfo.deviceOnline ? "Yes" : "No"}</p>
+                                            <p id="tmvEngineRunning">Engine Running: {vehicleInfo.engineRunning ? "Yes" : "No"}</p>
                                         </div>
                                     </Card.Body>
                                 </Card>
                             </Col>
                         </Row>
-                        <Row className="mt-3">
+                        <Row id="tmvContactPoliceRow" className="mt-3">
                             <Col className="text-center">
                                 <Button
-                                    className="contact-police-button"
+                                    id="tmvContactPoliceButton"
+                                    variant="outline-danger"
                                     onClick={contactPolice}
+                                    disabled={!policeStations.some(station => station.nationalPhoneNumber)}
                                 >
-                                    Did you lose your vehicle? Click here to contact police immediately.
+                                    Contact Nearest Police Station
                                 </Button>
                             </Col>
                         </Row>
-                        <Row className="mt-3">
+                        <Row id="tmvTogglePoliceStationsRow" className="mt-3">
                             <Col className="text-center">
                                 <Button
+                                    style={{margin:'25px'}}
+                                    variant="outline-info"
+                                    id="tmvTogglePoliceStationsButton"
                                     className="show-police-stations-button"
                                     onClick={togglePoliceStations}
-                                    disabled={loading}
+                                    disabled={!vehicleInfo || loading}
                                 >
-                                    {showPoliceStations ? 'Hide nearest police stations' : 'Show nearest police stations'}
+                                    {showPoliceStations ? 'Hide Nearest Police Stations' : 'Show Nearest Police Stations'}
                                 </Button>
                             </Col>
                         </Row>
 
-                        {showPoliceStations && policeStations.length > 0 && (
-                            <Accordion defaultActiveKey="0" className="mt-3">
-                                {policeStations.map((station, index) => (
-                                    <Accordion.Item eventKey={String(index)} key={station.displayName.text}>
-                                        <Accordion.Header>{station.displayName.text}</Accordion.Header>
-                                        <Accordion.Body>
-                                            <p>Address: {station.formattedAddress?.text || 'Not available'}</p>
-                                            <p>Phone: {
-                                                station.internationalPhoneNumber?.text ||
-                                                station.nationalPhoneNumber?.text ||
-                                                'Not available'
-                                            }</p>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                ))}
+                        {showPoliceStations && (
+                            <Accordion id="tmvPoliceStationsAccordion" defaultActiveKey="" className="mt-3">
+                                {policeStations.length > 0 ? (
+                                    policeStations.map((station, index) => (
+                                        <Accordion.Item eventKey={String(index)} key={index}>
+                                            <Accordion.Header>{station.displayName.text || 'Unknown Station'}</Accordion.Header>
+                                            <Accordion.Body>
+                                                <p>Address: {station.formattedAddress || 'Address not available'}</p>
+                                                <p>Phone: {
+                                                    station.nationalPhoneNumber || 'Phone number not available'
+                                                }</p>
+                                            </Accordion.Body>
+                                        </Accordion.Item>
+                                    ))
+                                ) : (
+                                    <p className="text-center">No police stations found.</p>
+                                )}
                             </Accordion>
-                            )}
+                        )}
                     </>
                 )}
-
-                {alertInfo.show && (
-                    <CustomAlert
-                        show={alertInfo.show}
-                        handleClose={handleCloseAlert}
-                        error={alertInfo.error}
-                    />
-                )}
             </Container>
+
+
+            {alertInfo.show && (
+                <CustomAlert
+                    show={alertInfo.show}
+                    handleClose={handleCloseAlert}
+                    error={alertInfo.error}
+                />
+            )}
         </>
     );
 }
